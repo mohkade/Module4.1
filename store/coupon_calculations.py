cbfc22594781a77288bc867a5787390f697df08c
Kaderhome2 <mmkader@dmacc.edu>
2020-02-12 21:46:45 -0600

    comit

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,4 +1,5 @@
 def calculate_price():
+#def calculate_price(price, cash_coupon, percent_coupon):
 #    price = float(input("Input price:"))
 #    cash_coupon = float(input("Input cash coupon amount:"))
 #    percent_coupon = float(input("Input percent coupon:"))
@@ -10,9 +11,36 @@ def calculate_price():
 #    final = int(round(final_price, 2))
 #    return final_price
 
+#try
+#cc_discount_price = price - cash_coupon
+#if cc_discount_price <= 0:
+    #return 0.00
+
+#percent_value = percent_coupon/100
+
+#if percent_value < 0:
+
+#discounted_value = cc_discount_price * (1 - percent_value)
+
+#except TypeError:
+ #   print('A typeError occured. Invalid input')
+#except:
+    #print('something else happened')
+#(not needed)finally:
+
+
+#return discounted_value
+#return((price - cash_coupon) * (1 - percent_coupon/100))
+
+   # def price():
     price = int(input("Input price:"))
+  #      return price
+ #   def cash_coupon():
     cash_coupon = int(input("Input cash coupon amount:"))
+#        return cash_coupon
+#    def percent_coupon():
     percent_coupon = int(input("Input percent coupon:"))
+#        return percent_coupon
     tax = 6/100
     new_price = (price - cash_coupon)
     new_price2 = (new_price / (1 + (percent_coupon / 100)))
@@ -20,13 +48,25 @@ def calculate_price():
     final_price = new_price2 * (1+tax)
 #    final = int(round(final_price, 2))
     return final_price
+
 if __name__ == '__main__':
 
+#    calculate_price()
     ship1 = 5.95
     ship2 = 7.95
     ship3 = 11.95
     ship4 = 0.00
-    final = int(calculate_price())
-    if final <= 10.00:
-        print(float(round(final, 2) + ship1))
+    final_price = float(calculate_price())
+    if final_price <= 10.00:
+        price1 = (float(round(final_price, 2) + ship1))
+        print("Your total is ${} includes shipping cost of ${}".format(price1, ship1))
+    elif 10.01 < final_price <= 30.00:
+        price2 = (float(round(final_price, 2) + ship2))
+        print("Your total is ${} includes shipping cost of ${}".format(price2, ship2))
+    elif 30.01< final_price <= 50.00:
+        price3 = (float(round(final_price, 2) + ship3))
+        print("Your total is ${} includes shipping cost of ${}".format(price3, ship3))
+    else:
+        price4 = (float(round(final_price, 2)))
+        print("Your total is ${} with free shipping!".format(price4))
 
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -1,26 +1,43 @@
 import unittest
+import unittest.mock as mock
+from store import coupon_calculaions as coupon
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
-    Test set 1:, price
-    under
-    10(test_price_under_ten)
-    price: under
-    10, 5
-    cash, 10 %
-    price: under
-    10, 5
-    cash, 15 %
-    price: under
-    10, 5
-    cash, 20 %
-    price: under
-    10, 10
-    cash, 10 %
+class FunctionTestCase(unittest.TestCase):
+    def test_price_under_ten(self):
+#        pass
+        under_ten = 7.00
 
+        with mock.patch('builtins.input', side_effects=[7.00,5.00,10]):
+            assert coupon.calculate_price () == 1.80
+        #price: under 10, 5 cash, 10%
+#        self.assertEqual(coupon.calculate_price(under_ten, 5, 10),1.8)
+        # price: under 10, 5 cash, 15%
+  #      self.assertEqual(coupon.calculate_price(under_ten, 5, 15),1.70)
+        # price: under 10, 5 cash, 20%
+   #     self.assertEqual(coupon.calculate_price(under_ten, 5, 20), 1.60)
+        # price: under 10, 5 cash, 25%
+    #    self.assertEqual(coupon.calculate_price(under_ten, 5, 25), 1.60)
+        # price: under 10, 10 cash, 10%
+     #   self.assertEqual(coupon.calculate_price(under_ten, 10, 10), answer)
+
+      #  self.assertEqual(True, False)
+
+#$15.99 - 5.00 = 10.99
+#get price of item
+#subtract coupon
+#discount
 
 if __name__ == '__main__':
-    unittest.main()
+    pass
+unittest.main()
+
+
+
+#def test_set_1(self):
+ #   dollar_amt = 7.00
+  #  assert coupons.calculate.print(dollar_amt, 5, 10) == 1.80
+
+
+
